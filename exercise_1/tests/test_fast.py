c12696dfd143820fc37c2b0016d4f574ee2d2564
tsion-19 <83315934+tsion-19@users.noreply.github.com>
2023-10-23 10:05:23 +0300

    test_fast.py

--- exercise_1/tests/test_fast.py
+++ exercise_1/tests/test_fast.py
@@ -1,4 +1,5 @@
 from intermediate_test_app import very_complex_logic
 
+
 def test_complex_logic_very_very_fast():
-	assert very_complex_logic(1,2,3) == 6
+    ssert very_complex_logic(1, 2, 3) == 6
