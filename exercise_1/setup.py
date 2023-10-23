4e4569026766014f23b0409cdc3aac7ce063d35e
tsion-19 <83315934+tsion-19@users.noreply.github.com>
2023-10-23 10:03:42 +0300

    setup.py

--- exercise_1/setup.py
+++ exercise_1/setup.py
@@ -5,10 +5,7 @@ from setuptools import setup, find_packages
 setup(
     name='intermediate_test_dummy_package',
     version='1.0.0',
-#    url='https://github.com/mypackage.git',
-#    author='Author Name',
-#    author_email='author@gmail.com',
-#    description='Description of my package',
+
     packages=['intermediate_test_app'],
     install_requires=[],
 )
